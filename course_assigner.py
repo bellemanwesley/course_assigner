ca18995840a697affbee8af3349c2447ca87c1fd
Wesley Belleman <bellemanwesley@gmail.com>
2023-10-26 00:07:12 -0400

    good for tonight

--- course_assigner.py
+++ course_assigner.py
@@ -1,11 +1,5 @@
 import random
 
-def assign(courses: dict, preferences: dict):
-    for x in preferences:
-        assert len(preferences[x])==len(courses),"Must list a preference for all courses"
-
-    return courses,preferences
-
 def random_assign(courses: dict,preferences: dict):
     result = {}
     for x in preferences:
@@ -19,3 +13,194 @@ def average_preference(assigned: dict,preferences: dict):
         denom += 1
         num += (preferences[x].index(assigned[x])+1)
     return num/denom
+
+def standard_deviation_preference(assigned: dict,preferences: dict):
+    num = 0
+    denom = 0
+    for x in preferences:
+        denom += 1
+        num += (preferences[x].index(assigned[x])+1)
+    average = num/denom    
+    num = 0
+    for x in preferences:
+        num += (preferences[x].index(assigned[x])+1-average)**2
+    return (num/denom)**0.5
+
+def simple_assign(courses: dict,preferences: dict):
+    for x in preferences:
+        assert len(preferences[x])==len(courses),"Must list a preference for all courses"
+    for x in courses:
+        assert courses[x]["min"]<=courses[x]["max"],"Minimum must be less than or equal to maximum"
+        assert courses[x]["min"]>=0,"Minimum must be greater than or equal to zero"  
+    #Assert that the sum of the maximums must be greater than or equal to the total number of students
+    assert sum([courses[x]["max"] for x in courses])>=len(preferences),"Sum of maximums must be greater than or equal to the total number of students"
+
+    #Assert that the sum of the minimums must be less than or equal to the total number of students 
+    assert sum([courses[x]["min"] for x in courses])<=len(preferences),"Sum of minimums must be less than or equal to the total number of students"
+
+    assigned = {}
+    unassigned_students = [x for x in preferences]
+
+    students_needed = {x:courses[x]["min"] for x in courses}
+
+    for i in range(len(courses)):
+        assigned_student_index = []
+        for j in range(len(unassigned_students)):
+            if students_needed[preferences[unassigned_students[j]][i]]>0:
+                assigned[unassigned_students[j]] = preferences[unassigned_students[j]][i]
+                students_needed[preferences[unassigned_students[j]][i]] -= 1
+                assigned_student_index.append(j)
+        #Sort in reverse order so that we can delete from the end of the list
+        assigned_student_index.sort(reverse=True)
+        for j in assigned_student_index:
+            del unassigned_students[j]
+
+    spaces_available = {x:courses[x]["max"]-len([y for y in assigned if assigned[y]==x]) for x in courses}
+    for i in range(len(courses)):
+        assigned_student_index = []
+        for j in range(len(unassigned_students)):
+            if spaces_available[preferences[unassigned_students[j]][i]]>0:
+                assigned[unassigned_students[j]] = preferences[unassigned_students[j]][i]
+                spaces_available[preferences[unassigned_students[j]][i]] -= 1
+                assigned_student_index.append(j)
+        #Sort in reverse order so that we can delete from the end of the list
+        assigned_student_index.sort(reverse=True)
+        for j in assigned_student_index:
+            del unassigned_students[j]
+    
+    if len(unassigned_students)>0:
+        print("Spaces Available:",spaces_available)
+        for student in unassigned_students:
+            print("Student",student,"was not assigned to a course.")
+            print("Student preferences:",preferences[student])
+    assert len(unassigned_students)==0,"Not all students were assigned due to an unexpected error."
+    for course in courses:
+        assert len([x for x in assigned if assigned[x]==course])<=courses[course]["max"],"Too many students were assigned to "+course
+        assert len([x for x in assigned if assigned[x]==course])>=courses[course]["min"],"Too few students were assigned to "+course
+    return assigned
+
+def reverse_simple_assign(courses: dict,preferences: dict):
+    for x in preferences:
+        assert len(preferences[x])==len(courses),"Must list a preference for all courses"
+    for x in courses:
+        assert courses[x]["min"]<=courses[x]["max"],"Minimum must be less than or equal to maximum"
+        assert courses[x]["min"]>=0,"Minimum must be greater than or equal to zero"  
+    #Assert that the sum of the maximums must be greater than or equal to the total number of students
+    assert sum([courses[x]["max"] for x in courses])>=len(preferences),"Sum of maximums must be greater than or equal to the total number of students"
+
+    #Assert that the sum of the minimums must be less than or equal to the total number of students 
+    assert sum([courses[x]["min"] for x in courses])<=len(preferences),"Sum of minimums must be less than or equal to the total number of students"
+
+    assigned = {}
+    unassigned_students = [x for x in preferences]
+
+    spaces_available = {x:courses[x]["max"]-len([y for y in assigned if assigned[y]==x]) for x in courses}
+    for i in range(len(courses)):
+        assigned_student_index = []
+        for j in range(len(unassigned_students)):
+            students_needed = {x:courses[x]["min"]-len([y for y in assigned if assigned[y]==x]) for x in courses}
+            total_students_needed = sum(students_needed[x] for x in students_needed if students_needed[x]>0)
+            if spaces_available[preferences[unassigned_students[j]][i]]>0 and total_students_needed<len(preferences)-len(assigned):
+                assigned[unassigned_students[j]] = preferences[unassigned_students[j]][i]
+                spaces_available[preferences[unassigned_students[j]][i]] -= 1
+                assigned_student_index.append(j)
+        #Sort in reverse order so that we can delete from the end of the list
+        assigned_student_index.sort(reverse=True)
+        for j in assigned_student_index:
+            del unassigned_students[j]
+
+    students_needed = {x:courses[x]["min"]-len([y for y in assigned if assigned[y]==x]) for x in courses}
+    for i in range(len(courses)):
+        assigned_student_index = []
+        for j in range(len(unassigned_students)):
+            if students_needed[preferences[unassigned_students[j]][i]]>0:
+                assigned[unassigned_students[j]] = preferences[unassigned_students[j]][i]
+                students_needed[preferences[unassigned_students[j]][i]] -= 1
+                assigned_student_index.append(j)
+        #Sort in reverse order so that we can delete from the end of the list
+        assigned_student_index.sort(reverse=True)
+        for j in assigned_student_index:
+            del unassigned_students[j]
+
+    if len(unassigned_students)>0:
+        print("Spaces Available:",spaces_available)
+        for student in unassigned_students:
+            print("Student",student,"was not assigned to a course.")
+            print("Student preferences:",preferences[student])
+    assert len(unassigned_students)==0,"Not all students were assigned due to an unexpected error."
+    for course in courses:
+        assert len([x for x in assigned if assigned[x]==course])<=courses[course]["max"],"Too many students were assigned to "+course
+        assert len([x for x in assigned if assigned[x]==course])>=courses[course]["min"],"Too few students were assigned to "+course
+    return assigned
+
+def compare_assign(courses: dict, preferences: dict):
+    for x in preferences:
+        assert len(preferences[x])==len(courses),"Must list a preference for all courses"
+    for x in courses:
+        assert courses[x]["min"]<=courses[x]["max"],"Minimum must be less than or equal to maximum"
+        assert courses[x]["min"]>=0,"Minimum must be greater than or equal to zero"  
+    #Assert that the sum of the maximums must be greater than or equal to the total number of students
+    assert sum([courses[x]["max"] for x in courses])>=len(preferences),"Sum of maximums must be greater than or equal to the total number of students"
+
+    #Assert that the sum of the minimums must be less than or equal to the total number of students 
+    assert sum([courses[x]["min"] for x in courses])<=len(preferences),"Sum of minimums must be less than or equal to the total number of students"
+
+    assigned = {}
+    unassigned_students = [x for x in preferences]
+
+    students_needed = {x:courses[x]["min"] for x in courses}
+
+    for i in range(len(courses)):
+        for course in courses:
+            assigned_student_index = []
+            #Determine the number of students who have course as their ith preference
+            potential_students = [x for x in unassigned_students if preferences[x][i]==course]
+            if students_needed[course]>len(potential_students) or i==len(courses)-1:
+                for j in range(len(unassigned_students)):
+                    if students_needed[preferences[unassigned_students[j]][i]]>0:
+                        assigned[unassigned_students[j]] = preferences[unassigned_students[j]][i]
+                        students_needed[preferences[unassigned_students[j]][i]] -= 1
+                        assigned_student_index.append(j)
+            else:
+                second_preferences = {}
+                for student in potential_students:
+                    for k in range(len(preferences[student])):
+                        if students_needed[preferences[student][k]]>0 and preferences[student][k]!=course:
+                            second_preferences[student] = preferences[student][k]
+                            break                
+                for j in range(students_needed[course]):
+                    highest_value = max(second_preferences.values())
+                    highest_student = [x for x in second_preferences if second_preferences[x]==highest_value][0]
+                    assigned[highest_student] = course
+                    students_needed[course] -= 1
+                    del second_preferences[highest_student]
+                    assigned_student_index.append(unassigned_students.index(highest_student))
+            #Sort in reverse order so that we can delete from the end of the list
+            assigned_student_index.sort(reverse=True)
+            for j in assigned_student_index:
+                del unassigned_students[j]
+
+
+    spaces_available = {x:courses[x]["max"]-len([y for y in assigned if assigned[y]==x]) for x in courses}
+    for i in range(len(courses)):
+        assigned_student_index = []
+        for j in range(len(unassigned_students)):
+            if spaces_available[preferences[unassigned_students[j]][i]]>0:
+                assigned[unassigned_students[j]] = preferences[unassigned_students[j]][i]
+                spaces_available[preferences[unassigned_students[j]][i]] -= 1
+                assigned_student_index.append(j)
+        #Sort in reverse order so that we can delete from the end of the list
+        assigned_student_index.sort(reverse=True)
+        for j in assigned_student_index:
+            del unassigned_students[j]
+    
+    if len(unassigned_students)>0:
+        print("Spaces Available:",spaces_available)
+        for student in unassigned_students:
+            print("Student",student,"was not assigned to a course.")
+            print("Student preferences:",preferences[student])
+    assert len(unassigned_students)==0,"Not all students were assigned due to an unexpected error."
+    for course in courses:
+        assert len([x for x in assigned if assigned[x]==course])<=courses[course]["max"],"Too many students were assigned to "+course
+        assert len([x for x in assigned if assigned[x]==course])>=courses[course]["min"],"Too few students were assigned to "+course
+    return assigned
--- tests/against_random.py
+++ tests/against_random.py
@@ -1,5 +1,9 @@
-import course_assigner
 import random
+import time
+#Import the script located at ../course_assigner.py
+import sys
+sys.path.append("..")
+import course_assigner
 
 courses_list = [
     "Spanish",
@@ -7,7 +11,10 @@ courses_list = [
     "Geometry",
     "Biology",
     "History",
-    "English"
+    "English",
+    "Chemistry",
+    "Physics",
+    "Calculus"
 ]
 
 courses = {}
@@ -15,13 +22,42 @@ courses = {}
 for x in courses_list:
     courses[x] = {}
     courses[x]["min"] = random.randint(0,1000//len(courses_list))
-    courses[x]["max"] = random.randint(courses[x]["min"],1000)
+    courses[x]["max"] = random.randint(max(courses[x]["min"],300),1000)
 
 preferences = {}
 
-for i in range(1000):
+for i in range(4000):
     random.shuffle(courses_list)
     preferences[i] = courses_list
 
-assigned = course_assigner.random_assign(courses=courses,preferences=preferences)
-average_preference = course_assigner.average_preference(assigned=assigned,preferences=preferences)
+start = time.time()
+assigned_random = course_assigner.random_assign(courses=courses,preferences=preferences)
+end = time.time()
+time_random = end-start
+average_preference_random = course_assigner.average_preference(assigned=assigned_random,preferences=preferences)
+standard_deviation_preference_random = course_assigner.standard_deviation_preference(assigned=assigned_random,preferences=preferences)
+print("_RANDOM_","Average:",average_preference_random,"Standard Deviation:",standard_deviation_preference_random,"Time:",time_random)
+
+start = time.time()
+assigned_simple = course_assigner.simple_assign(courses=courses,preferences=preferences)
+end = time.time()
+time_simple = end-start
+average_preference_simple = course_assigner.average_preference(assigned=assigned_simple,preferences=preferences)
+standard_deviation_preference_simple = course_assigner.standard_deviation_preference(assigned=assigned_simple,preferences=preferences)
+print("_SIMPLE_","Average:",average_preference_simple,"Standard Deviation:",standard_deviation_preference_simple,"Time:",time_simple)
+
+start = time.time()
+assigned_reverse_simple = course_assigner.reverse_simple_assign(courses=courses,preferences=preferences)
+end = time.time()
+time_reverse_simple = end-start
+average_preference_reverse_simple = course_assigner.average_preference(assigned=assigned_reverse_simple,preferences=preferences)
+standard_deviation_preference_reverse_simple = course_assigner.standard_deviation_preference(assigned=assigned_reverse_simple,preferences=preferences)
+print("_REVERSE_SIMPLE_","Average:",average_preference_reverse_simple,"Standard Deviation:",standard_deviation_preference_reverse_simple,"Time:",time_reverse_simple)
+
+start = time.time()
+assigned_compare = course_assigner.compare_assign(courses=courses,preferences=preferences)
+end = time.time()
+time_compare = end-start
+average_preference_compare = course_assigner.average_preference(assigned=assigned_compare,preferences=preferences)
+standard_deviation_preference_compare = course_assigner.standard_deviation_preference(assigned=assigned_compare,preferences=preferences)
+print("_COMPARE_","Average:",average_preference_compare,"Standard Deviation:",standard_deviation_preference_compare,"Time:",time_compare)
